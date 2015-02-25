2d3afc6c4dd7c50e67727404f3c7e8fa573d6cca
hakonsbm <h.s.b.mork@fys.uio.no>
2015-02-25 16:49:09 +0100

    vmc_IS now goes over different dt

--- source/main_IS.cpp
+++ source/main_IS.cpp
@@ -18,43 +18,34 @@ int main() {
 
     //Opens the file that the relevant wavefunction should be written to, this file is then written to in the
     //vmcSolver class
-    char const * outfilePath = (string("../source/outfiles/") + solver->trialFunction()->m_outfileName+string("_IS_1.7")).c_str();
+    char const * outfilePath = (string("../source/outfiles/") + solver->trialFunction()->m_outfileName+string("_IS_dt_0.1to1.0")).c_str();
     outfile.open(outfilePath);
 
 
     double alpha_max = 0.85*solver->getCharge();
-    double beta_max = 1.5;
+    double alpha_min = 0.85*solver->getCharge();
     double d_alpha = 0.1;
+
+    double beta_max = 1.5;
+    double beta_min = 1.01;
     double d_beta = 0.01;
+
+    double dt_min = 0.05;
+    double dt_max = 3.0;
+    double d_dt = 0.05;
 
     clock_t start, end;     //To keep track of the time
 
-
-    for(double alpha = 0.85*solver->getCharge(); alpha <= alpha_max; alpha += d_alpha) {
-        solver->setAlpha(alpha);
-        if(solver->trialFunction()->simpleFlag) {
-
-            start = clock();
-                solver->calculateOptimalSteplength();
-            end = clock();
-
-            double timeOptimalStepLength = 1.0*(end - start)/CLOCKS_PER_SEC;
-
-            start = clock();
-                solver->runMonteCarloIntegration();
-            end = clock();
-
-            double timeRunMonte= 1.0*(end - start)/CLOCKS_PER_SEC;
-
-            cout << "Time to find Optimal Steplength: " << timeOptimalStepLength << endl;
-            cout << "Time to run Monte Carlo: " << timeRunMonte << endl;
-        }
-        else {
-            for(double beta = 1.01; beta <= beta_max; beta += d_beta) {
-                solver->setBeta(beta);
+    for(double dt = dt_min; dt <= dt_max; dt += d_dt){
+        solver->setSteplength(dt);
+        //solver -> stepLength = dt;
+        cout << "\ndt = " << dt << endl;
+        for(double alpha = alpha_min; alpha <= alpha_max; alpha += d_alpha) {
+            solver->setAlpha(alpha);
+            if(solver->trialFunction()->simpleFlag) {
 
                 start = clock();
-                    solver->calculateOptimalSteplength();
+                    //solver->calculateOptimalSteplength();
                 end = clock();
 
                 double timeOptimalStepLength = 1.0*(end - start)/CLOCKS_PER_SEC;
@@ -65,10 +56,30 @@ int main() {
 
                 double timeRunMonte= 1.0*(end - start)/CLOCKS_PER_SEC;
 
-                cout << "Time to find Optimal Steplength: " << timeOptimalStepLength << endl;
+                //cout << "Time to find Optimal Steplength: " << timeOptimalStepLength << endl;
                 cout << "Time to run Monte Carlo: " << timeRunMonte << endl;
+            }
+            else {
+                for(double beta = beta_min; beta <= beta_max; beta += d_beta) {
+                    solver->setBeta(beta);
+
+                    start = clock();
+                        //solver->calculateOptimalSteplength();
+                    end = clock();
+
+                    double timeOptimalStepLength = 1.0*(end - start)/CLOCKS_PER_SEC;
+
+                    start = clock();
+                        solver->runMonteCarloIntegration();
+                    end = clock();
+
+                    double timeRunMonte= 1.0*(end - start)/CLOCKS_PER_SEC;
+
+                    //cout << "Time to find Optimal Steplength: " << timeOptimalStepLength << endl;
+                    cout << "Time to run Monte Carlo: " << timeRunMonte << endl;
 
 
+                }
             }
         }
     }
