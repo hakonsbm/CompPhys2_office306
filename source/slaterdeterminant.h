4b7cc365ed43c3724e5357269933e976caaea39b
Emilio <joseer@ulrik.uio.no>
2015-04-16 12:38:31 +0200

    added determinant ratios

--- source/slaterdeterminant.h
+++ source/slaterdeterminant.h
@@ -1,6 +1,8 @@
 #ifndef SLATERDETERMINANT_H
 #define SLATERDETERMINANT_H
 #include <armadillo>
+#include "vmcsolver.h"
+#include "derivatives.h"
 
 
 using namespace arma;
@@ -15,6 +17,10 @@ public:
 
     double calculateDeterminant(const mat &r,double alpha, VMCSolver *solver);
     double phi(const mat &r, double alpha, int i, int j, VMCSolver *solver);
+    double SlaterDeterminant::determinantRatioUp(const mat &r, VMCSolver *solver, Derivatives *der);
+    double SlaterDeterminant::determinantRatioDown(const mat &r, VMCSolver *solver, Derivatives *der);
+    double SlaterDeterminant::determinantLaplacianRatioUp(const mat &r, VMCSolver *solver, Derivatives *der);
+    double SlaterDeterminant::determinantLaplacianRatioDown(const mat &r, VMCSolver *solver, Derivatives *der);
 
     mat detUp;
     mat detDown;
