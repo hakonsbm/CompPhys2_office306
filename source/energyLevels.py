6df9eb09bd75678163a204ccf822c2c7d40aaf9f
Gullik Vetvik Killie <gullik.killie@gmail.com>
2015-05-26 13:11:49 +0200

    Working on atoms

--- source/energyLevels.py
+++ source/energyLevels.py
@@ -88,7 +88,7 @@ def plotEnergyVsAlphaBeta(data, name):
 
 	fig.savefig("../Report/figures/" + name + "AlphaBetaVariance")
 
-	findLowestEnergy(data)
+	findLowestEnergy(data, name)
 
 
 def plotEnergyVsAlpha(data, name):
@@ -241,24 +241,24 @@ def plotVarVSnCycles(data, name):
 #Decide what we want to plot this time
 
 # name = "HeliumSimpleAnalytical"
-# name = "HeliumJastrowAnalytical"
-name = "Beryllium"
+name = "HeliumJastrowAnalytical"
+# name = "Beryllium"
 # name = "Neon"
 
 
 
-# data = np.genfromtxt("outfiles/" + name + "_alpha_beta")
+data = np.genfromtxt("outfiles/" + name + "_alpha_beta")
 # datatime = np.genfromtxt("outfiles/" + name +"_timeStep")
-dataSample = np.genfromtxt("outfiles/" + name +"_blockingSamples")
+# dataSample = np.genfromtxt("outfiles/" + name +"_blockingSamples")
 # dataCycles = np.genfromtxt("outfiles/" + name +"_nCycles")
 
 
 # findLowestEnergy(data, name)
 # plotResultsVSTimestep(datatime , name)
-# plotEnergyVsAlphaBeta(data, name)
+plotEnergyVsAlphaBeta(data, name)
 # plotEnergyVsAlpha(data, name)
 # plotResultsVSTimestep(datatime , name)
-plotChargeDensity(dataSample, name)
+# plotChargeDensity(dataSample, name)
 # plotVarVSnCycles(dataCycles[1:,:], name)
 
 
